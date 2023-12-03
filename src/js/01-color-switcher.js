f824727193890993912e61d3d7ee583866ba5979
amending1 <joasia.sowiak@gmail.com>
2023-12-03 22:49:51 +0100

    changes

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,30 +2,29 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-document.querySelector('[data-start]').addEventListener('click', startColorChange);
-document.querySelector('[data-stop]').addEventListener('click', stopColorChange);
+document
+  .querySelector('[data-start]')
+  .addEventListener('click', startColorChange);
+document
+  .querySelector('[data-stop]')
+  .addEventListener('click', stopColorChange);
 let currentColor;
-  
-function startColorChange() {
-  const startButton = document.querySelector('[data-start]');
-  const stopButton = document.querySelector('[data-stop]');
 
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+
+function startColorChange() {
   startButton.disabled = true;
   stopButton.disabled = false;
 
-  
-    currentColor = setInterval(() => {
+  currentColor = setInterval(() => {
     document.body.style.backgroundColor = getRandomHexColor();
   }, 1000);
 }
 
 function stopColorChange() {
-  const startButton = document.querySelector('[data-start]');
-  const stopButton = document.querySelector('[data-stop]');
-
   startButton.disabled = false;
   stopButton.disabled = true;
 
   clearInterval(currentColor);
 }
-
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,12 +1,3 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
-
 document.addEventListener('DOMContentLoaded', function () {
   const form = document.querySelector('.form');
 
